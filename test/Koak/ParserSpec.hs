498c23f8b3f179d1f7211d68807de2f8cf9edc8b
matheo lucak <matheo.lucak@epitech.eu>
2022-02-22 18:36:01 +0100

    [ADD] Unit tests for expression call

--- test/Koak/ParserSpec.hs
+++ test/Koak/ParserSpec.hs
@@ -54,7 +54,177 @@ spec = do
                             []
                         )
                     )
-            ]
+                ]
+    it "Simple function call without args: foo();" $ do
+        parseKoak [
+            KL.Word "foo",
+            KL.OpenParenthesis,
+            KL.ClosedParenthesis,
+            KL.SemiColon]
+            == [
+                KP.KDEFS_EXPR
+                    (EXPRESSIONS
+                        (EXPRESSION
+                            (UNARY_POSTFIX
+                                (POSTFIX
+                                    (PRIMARY_IDENTIFIER
+                                        (IDENTIFIER "foo")
+                                    )
+                                    (Just (CALL_EXPR
+                                        (Nothing)
+                                    ))
+                                )
+                            )
+                            []
+                        )
+                        []
+                    )
+                ]
+    it "Simple function call with one number as argument: foo(1);" $ do
+        parseKoak [
+            KL.Word "foo",
+            KL.OpenParenthesis,
+            KL.ClosedParenthesis,
+            KL.SemiColon]
+            == [
+                KP.KDEFS_EXPR
+                    (EXPRESSIONS
+                        (EXPRESSION
+                            (UNARY_POSTFIX
+                                (POSTFIX
+                                    (PRIMARY_IDENTIFIER
+                                        (IDENTIFIER "foo")
+                                    )
+                                    (Just (CALL_EXPR
+                                        (Just (CALL_EXPR_ARGS
+                                            (EXPRESSION
+                                                (UNARY_POSTFIX
+                                                    (POSTFIX
+                                                        (PRIMARY_LITERAL
+                                                            (LITERAL_DECIMAL
+                                                                (DECIMAL_CONST 1)
+                                                            )
+                                                        )
+                                                        Nothing
+                                                    )
+                                                )
+                                                []
+                                            )
+                                            []
+                                        ))
+                                    ))
+                                )
+                            )
+                            []
+                        )
+                        []
+                    )
+                ]
+    it "Simple function call with one variable as argument: foo(my_var);" $ do
+        parseKoak [
+            KL.Word "foo",
+            KL.OpenParenthesis,
+            KL.ClosedParenthesis,
+            KL.SemiColon]
+            == [
+                KP.KDEFS_EXPR
+                    (EXPRESSIONS
+                        (EXPRESSION
+                            (UNARY_POSTFIX
+                                (POSTFIX
+                                    (PRIMARY_IDENTIFIER
+                                        (IDENTIFIER "foo")
+                                    )
+                                    (Just (CALL_EXPR
+                                        (Just (CALL_EXPR_ARGS
+                                            (EXPRESSION
+                                                (UNARY_POSTFIX
+                                                    (POSTFIX
+                                                        (PRIMARY_IDENTIFIER
+                                                            (IDENTIFIER "my_var")
+                                                        )
+                                                        Nothing
+                                                    )
+                                                )
+                                                []
+                                            )
+                                            []
+                                        ))
+                                    ))
+                                )
+                            )
+                            []
+                        )
+                        []
+                    )
+                ]
+    it "Simple function call with multiple primary arguments: foo(1, my_var, 3);" $ do
+        parseKoak [
+            KL.Word "foo",
+            KL.OpenParenthesis,
+            KL.ClosedParenthesis,
+            KL.SemiColon]
+            == [
+                KP.KDEFS_EXPR
+                    (EXPRESSIONS
+                        (EXPRESSION
+                            (UNARY_POSTFIX
+                                (POSTFIX
+                                    (PRIMARY_IDENTIFIER
+                                        (IDENTIFIER "foo")
+                                    )
+                                    (Just (CALL_EXPR
+                                        (Just (CALL_EXPR_ARGS
+                                            (EXPRESSION
+                                                (UNARY_POSTFIX
+                                                    (POSTFIX
+                                                        (PRIMARY_LITERAL
+                                                            (LITERAL_DECIMAL
+                                                                (DECIMAL_CONST 1)
+                                                            )
+                                                        )
+                                                        Nothing
+                                                    )
+                                                )
+                                                []
+                                            )
+                                            [
+                                                (EXPRESSION
+                                                    (UNARY_POSTFIX
+                                                        (POSTFIX
+                                                            (PRIMARY_IDENTIFIER
+                                                                (IDENTIFIER "my_var")
+                                                            )
+                                                            Nothing
+                                                        )
+                                                    )
+                                                    []
+                                                ),
+                                                (EXPRESSION
+                                                    (UNARY_POSTFIX
+                                                        (POSTFIX
+                                                            (PRIMARY_LITERAL
+                                                                (LITERAL_DECIMAL
+                                                                    (DECIMAL_CONST 3)
+                                                                )
+                                                            )
+                                                            Nothing
+                                                        )
+                                                    )
+                                                    []
+                                                )    
+                                            ]
+                                        ))
+                                    ))
+                                )
+                            )
+                            []
+                        )
+                        []
+                    )
+                ]
+
+-- foo(): bar(2): foobar(1, 2, 3)
 
 -- def foo () : int 42;
 -- test (5.0) - 2 * 3 + 1;
