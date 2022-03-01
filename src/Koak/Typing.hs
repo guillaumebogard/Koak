740ece4eafb5ff995b04c9558977bfaa68f55599
matheo lucak <matheo.lucak@epitech.eu>
2022-03-01 14:07:15 +0100

    [WIP] Base for typing

--- src/Koak/Typing.hs
+++ src/Koak/Typing.hs
@@ -35,11 +35,23 @@ import Koak.Parser          ( KDEFS(..)
                             , LITERAL(..)
                             )
 
-checkKoakTyping :: [KDEFS] -> () 
+newtype VAR_FRAME  = VAR_FRAME [(IDENTIFIER, TYPE)]
+
+type VAR_FRAME_STACK = [VAR_FRAME]
+
+data VAR_CONTEXT = VAR_CONTEXT [KDEFS] VAR_FRAME_STACK
+
+-- Au lieU de se balader avec plein de KDEF, on va se balader avec la structure suivante
+-- La structure est pas fini, mais elle contient toute les [KDEFS], et la liste de variables locales déclarés, comme une stack
+-- Et il faudra faire une fonction pour push et pop des variables et des stack frame
+
+-- J'utilise l'op <-> pour chainer mes appelle de check___Typing
+    
+checkKoakTyping :: [KDEFS] -> ()
 checkKoakTyping _ = ()
 -- checkKoakTyping kdefs = map checkKdefTyping kdefs
 
-checkKdefTyping :: [KDEFS] -> KDEFS -> () 
+checkKdefTyping :: [KDEFS] -> KDEFS -> ()
 checkKdefTyping kdefs (KDEFS_DEFS defs)  = checkDefsTyping kdefs defs
 checkKdefTyping kdefs (KDEFS_EXPR exprs) = checkExpressionsTyping kdefs exprs
 
@@ -47,19 +59,29 @@ checkDefsTyping :: [KDEFS] -> DEFS -> ()
 checkDefsTyping _ _ = ()
 
 checkExpressionsTyping :: [KDEFS] -> EXPRESSIONS -> ()
-checkExpressionsTyping kdefs (FOR_EXPR    expr)       = ()
-checkExpressionsTyping kdefs (IF_EXPR     expr)       = ()
-checkExpressionsTyping kdefs (WHILE_EXPR  expr)       = ()
-checkExpressionsTyping kdefs (EXPRESSIONS expr exprs) = ()
+checkExpressionsTyping kdefs (FOR_EXPR    for_expr  ) = checkForTyping   kdefs for_expr
+checkExpressionsTyping kdefs (IF_EXPR     if_expr   ) = checkIfTyping    kdefs if_expr
+checkExpressionsTyping kdefs (WHILE_EXPR  while_expr) = checkWhileTyping kdefs while_expr
+checkExpressionsTyping kdefs (EXPRESSIONS expr exprs) = -- checkExpressionTyping kdefs expr <-> -- pas fini
+
+checkExpressionTyping :: [KDEFS] -> EXPRESSION -> ()
+checkExpressionTyping kdefs _= ()
 
 checkForTyping :: [KDEFS] -> FOR -> ()
-checkForTyping kdefs (FOR _ _ _ _ _ exprs) = checkExpressionsTyping kdefs exprs
+checkForTyping kdefs (FOR assign_id assign_expr cmp_id cmp_expr inc_expr exprs) = checkExpressionsTyping kdefs exprs
 
 checkIfTyping :: [KDEFS] -> IF -> ()
-checkIfTyping kdefs (IF _ exprs _) = checkExpressionsTyping kdefs exprs
+checkIfTyping kdefs (IF cmp_expr then_exprs (Just else_exprs)) = checkExpressionTyping  kdefs cmp_expr   <->
+                                                                 checkExpressionsTyping kdefs then_exprs <->
+                                                                 checkExpressionsTyping kdefs else_exprs
+checkIfTyping kdefs (IF cmp_expr then_exprs Nothing         )  = checkExpressionTyping  kdefs cmp_expr   <->
+                                                                 checkExpressionsTyping kdefs then_exprs
 
 checkWhileTyping :: [KDEFS] -> WHILE -> ()
-checkWhileTyping kdefs (WHILE _ exprs) = checkExpressionsTyping kdefs exprs
+checkWhileTyping kdefs (WHILE expr exprs) = checkExpressionTyping  kdefs expr <->
+                                            checkExpressionsTyping kdefs exprs
+
+
 
 evaluateExpressionsType :: EXPRESSIONS -> TYPE
 evaluateExpressionsType (FOR_EXPR    expr)       = evaluateForType   expr
@@ -78,11 +100,11 @@ evaluateWhileType :: WHILE -> TYPE
 evaluateWhileType  (WHILE _ exprs) = evaluateExpressionsType exprs
 
 evaluateExpressionType :: EXPRESSION -> TYPE
-evaluateExpressionType = getLastOp . getFunctionType
+evaluateExpressionType _ = INT --getLastOp . getFunctionType
 
-getLastOp :: EXPRESSION -> TYPE
-getLastOp (EXPRESSION unary []) = getUnaryType unary
-getLastOp expr = getLastOp' expr IDENTIFIER ""
+-- getLastOp :: EXPRESSION -> TYPE
+-- getLastOp (EXPRESSION unary []) = getUnaryType unary
+-- getLastOp expr = getLastOp' expr IDENTIFIER ""
 
 
 --getLastOp' :: EXPRESSION -> IDENTIFIER -> IDENTIFIER 
@@ -91,10 +113,13 @@ getLastOp expr = getLastOp' expr IDENTIFIER ""
 getUnaryType :: UNARY -> TYPE
 getUnaryType = error "pute"
 
-getFunctionType :: [KDEFS] -> IDENTIFIER -> TYPE
-getFunctionType (KDEFS_DEFS ((PROTOTYPE identifier (PROTOTYPE_ARGS _ fnType))) :xs) functionName
-  | identifier == functionName = fnType
-  | otherwise                  = getFunctionType xs functionName
+-- getFunctionType :: [KDEFS] -> IDENTIFIER -> TYPE
+-- getFunctionType (KDEFS_DEFS ((PROTOTYPE identifier (PROTOTYPE_ARGS _ fnType))) :xs) functionName
+--   | identifier == functionName = fnType
+--   | otherwise                  = getFunctionType xs functionName
 
 
 -- find :: Foldable t => (a -> Bool) -> t a -> Maybe a
+
+(<->) :: () -> () -> ()
+(<->) _ _ = ()
